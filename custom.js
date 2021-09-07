70506198bf59b079578fc0e835a0658415113fab
ruchitamalviya <88784758+ruchitamalviya@users.noreply.github.com>
2021-09-07 20:28:03 +0530

    Add files via upload
    changes added

--- custom.js
+++ custom.js
@@ -1,26 +1,26 @@
-function userInputValue() {
+(function() {
+    var inputValue = document.getElementById("user_input");
 
-		var inputValue = document.getElementById("user_input");
-		var str = inputValue.value;
-		alphabetPosition(str);
-	}
+    function userInputValue() {
+        var str = inputValue.value;
+        var newlist = document.createElement('ul');
+        //alert(str);
+        alphabetPosition(str, newlist);
+    }
+    inputValue.onkeyup = userInputValue;
 
-	function alphabetPosition(text) {
+    function alphabetPosition(text, newlist) {
+        var result = "";
+        for (var i = 0; i < text.length; i++) {
+            var code = text.toUpperCase().charCodeAt(i);
+            if (code > 64 && code < 91) {
+                result = (code - 64) + " "
+                document.getElementById('newadded').appendChild(newlist).innerHTML = "<li>The index of " + text[i] + " " + result + "</li>";
+            } else {
+                document.getElementById('newadded').appendChild(newlist).innerHTML = "<li>" + text[i] + " is not an alphabet</li>";
 
-		var result = "";
-		for(var i = 0; i < text.length; i++) {
+            }
+        }
 
-			var code = text.toUpperCase().charCodeAt(i);
-		}
-				
-		if(code > 64 && code < 91) {
-			result += (code - 64) + " "
-			document.getElementById('indexValue').innerHTML = " The Index Of" + " " +result;
-
-			}else{
-
-			document.getElementById('indexValue').innerHTML = "The Input Is Not Alphabet";
-			}
-	
-		}
-
+    }
+})();
